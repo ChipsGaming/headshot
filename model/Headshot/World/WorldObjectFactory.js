ab639f9b045cbd568705a8e904bca80c9a58125c
artur <Artur-Mamedbekov@yandex.ru>
2017-03-19 02:05:21 +0300

    Fix

--- model/Headshot/World/WorldObjectFactory.js
+++ model/Headshot/World/WorldObjectFactory.js
@@ -12,17 +12,23 @@ define(function(require, exports, module){
         this.idIterator=0;
     }
 
-    WorldObjectFactory.prototype.create= function (id,enableTarget,type) {
-        return new WorldObject(id||this.idIterator++,
-                                type||this.Types.player, enableTarget);
+    WorldObjectFactory.prototype.Types={
+      player: 0,
+      barrier: 1
+    };
+
+    WorldObjectFactory.prototype.create= function (id, enableTarget, type) {
+      var id = id || this.idIterator++,
+        type = type || this.Types.player;
+
+      return new WorldObject(id, type, enableTarget);
     }
 
-    WorldObjectFactory.prototype.createFromBase=function (id, base, enableTarget,type ) {
-        var wObject= new WorldObject(id||this.idIterator++, enableTarget, type||this.Types.player);
-        return wObject.cloneTo(base);
+    WorldObjectFactory.prototype.createFromBase = function (id, base, enableTarget, type){
+      var wObject = new WorldObject(id, enableTarget, type);
+
+      return wObject.cloneTo(base);
     }
 
-    WorldObjectFactory.prototype.Types={player:0, barrier:1};
-    
     module.exports = WorldObjectFactory;
 });
