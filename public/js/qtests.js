6a0a1d85d5633441011de6a844f29af59c1292b8
Алавутдин Гебеков < >
2017-04-15 17:10:04 +0300

    Прикрутил простой инструмент модульных тестов

---
+++ public/js/qtests.js
@@ -0,0 +1,5 @@
+QUnit.test("WorldObject", function (assert) {
+  assert.equal( 0, 0, "Zero, Zero; equal succeeds" );
+  assert.equal( "", 0, "Empty, Zero; equal succeeds" );
+  assert.equal( "", "", "Empty, Empty; equal succeeds" );
+});
